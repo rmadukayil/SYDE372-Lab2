f25baf807da65f268d6116d56c1925b9c852fab9
rmadukayil <43508700+rmadukayil@users.noreply.github.com>
2020-03-22 23:14:39 -0400

    Completed parametric estimation

--- MLclassifier.m
+++ MLclassifier.m
@@ -1,15 +1,14 @@
 
 function [output] = MLclassifier(x, y, class12, class23, class13)
-
 	output = zeros(size(x,1), size(y,2));    
 	for i = 1:size(x,1)
         	for j = 1:size(y,2)
 			if class12(i,j) >= 0 && class23(i,j) <= 0
-				Z(i,j) = 1;
+				output(i,j) = 1;
 			elseif class23(i,j) >= 0 && class13(i,j) <= 0
-				Z(i,j) = 2;
+				output(i,j) = 2;
 			elseif class13(i,j) >= 0 && class12(i,j) <= 0
-				Z(i,j) = 3;
+				output(i,j) = 3;
 			end
 		end
 	end
--- section3.m
+++ section3.m
@@ -4,29 +4,29 @@ clc;
 
 % load data (2 column matrix)
 data = load('lab2_2');
-setA = data.al;
-setB = data.bl;
-setC = data.cl;
+al = data.al;
+bl = data.bl;
+cl = data.cl;
+at = data.at;
+bt = data.bt;
+ct = data.ct;
 
 % calculate known parameters for each set
-muA = mean(setA);
-covA = cov(setA); % check for unbiased est
-nA = length(setA);
-muB = mean(setB);
-covB = cov(setB);
-nB = length(setB);
-muC = mean(setC);
-covC = cov(setC);
-nC = length(setC);
+muA = mean(al);
+covA = cov(al); % check for unbiased est
+muB = mean(bl);
+covB = cov(bl);
+muC = mean(cl);
+covC = cov(cl);
 
 % mesh grid set-up
-x_min = min([setA(:,1); setB(:,1); setC(:,1)]); %adjust param later
-y_min = min([setA(:,2); setB(:,2); setC(:,2)]);
-x_max = max([setA(:,2); setB(:,2); setC(:,2)]);
-y_max = max([setA(:,2); setB(:,2); setC(:,2)]);
+x_min = min([al(:,1); bl(:,1); cl(:,1)]); %adjust param later
+y_min = min([al(:,2); bl(:,2); cl(:,2)]);
+x_max = max([al(:,1); bl(:,1); cl(:,1)]);
+y_max = max([al(:,2); bl(:,2); cl(:,2)]);
 
-x_range = x_min:1:x_max;
-y_range = y_min:1:y_max;
+x_range = x_min-2:1:x_max+2;
+y_range = y_min-2:1:y_max+2;
 
 [x, y] = meshgrid(x_range, y_range);
 
@@ -36,5 +36,14 @@ classBC_estimate = MLestimate(muB, covB, muC, covC, x, y);
 classAC_estimate = MLestimate(muA, covA, muC, covC, x, y);
 
 result_grid = MLclassifier(x, y, classAB_estimate, classBC_estimate, classAC_estimate);
+
+% plotting parametric est
+figure;
+hold on;
+scatter(al(:,1), al(:,2));
+scatter(bl(:,1), bl(:,2));
+scatter(cl(:,1), cl(:,2));
+contour(x,y,result_grid,'color', 'r');
+hold off;
 
 % non parametric estimation 
