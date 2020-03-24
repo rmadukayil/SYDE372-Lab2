016500a46279df7465a57fa9d3068076e832e02a
rmadukayil <43508700+rmadukayil@users.noreply.github.com>
2020-03-24 12:05:01 -0400

    Added plot titles for Sec3, added Sec3 to main
    TODO: fix main for part 4

--- main.m
+++ main.m
@@ -130,6 +130,95 @@ xlabel('x');
 ylabel('p(x)');
 legend('True p(x)','Estimated p(x) - Non-Parametric');
 hold off;
+
+%% PART 3
+data = load('lab2_2');
+Alearn = data.al;
+Blearn = data.bl;
+Clearn = data.cl;
+Atest = data.at;
+Btest = data.bt;
+Ctest = data.ct;
+
+% FOR PARAMETRIC ESTIMATION
+% calculate known parameters for each set
+muA = mean(Alearn);
+covA = cov(Alearn); % check for unbiased est
+muB = mean(Blearn);
+covB = cov(Blearn);
+muC = mean(Clearn);
+covC = cov(Clearn);
+
+% mesh grid set-up
+x_min = min([Alearn(:,1); Blearn(:,1); Clearn(:,1)]); %adjust param later
+y_min = min([Alearn(:,2); Blearn(:,2); Clearn(:,2)]);
+x_max = max([Alearn(:,1); Blearn(:,1); Clearn(:,1)]);
+y_max = max([Alearn(:,2); Blearn(:,2); Clearn(:,2)]);
+
+x_range1 = x_min-2:1:x_max+2;
+y_range1 = y_min-2:1:y_max+2;
+
+[x1, y1] = meshgrid(x_range1, y_range1);
+
+% parametric estimation w/ ML
+classAB = MLestimate(muA, covA, muB, covB, x1, y1);
+classBC = MLestimate(muB, covB, muC, covC, x1, y1);
+classAC = MLestimate(muA, covA, muC, covC, x1, y1);
+
+parametric_grid = MLclassifier(x1, y1, classAB, classBC, classAC);
+
+% plotting parametric estimate
+figure(11);
+hold on;
+title('Parametric Estimation for 2D Case');
+xlabel('x1');
+ylabel('x2');
+sz = 20;
+scatter(Atest(:, 1), Atest(:, 2), sz, 'filled');
+scatter(Btest(:, 1), Btest(:, 2), sz, 'filled');
+scatter(Ctest(:, 1), Ctest(:, 2), sz, 'filled');
+contour(x1,y1,parametric_grid,'color', 'black');
+legend('Class A', 'Class B', 'Class C', 'ML Decision Boundary');
+hold off;
+
+% FOR NON-PARAMETRIC ESTIMATION
+% given parameters 
+mean = [200 200];
+covariance = [400 0; 0 400];
+stepsize = 1; %adjust
+
+% setting up meshgrid, window
+[x2, y2] = meshgrid(0:1:400);
+pzn_win = mvnpdf([x2(:) y2(:)], mean, covariance);
+pzn_win = reshape(pzn_win,length(y2),length(x2));
+resolution = [stepsize x_min y_min x_max  y_max];
+
+% non-parametric estimate using given Parzen window code
+[p_a, x_a, y_a] = parzen(Alearn, resolution, pzn_win);
+[p_b, x_b, y_b] = parzen(Blearn, resolution, pzn_win);
+[p_c, x_c, y_c] = parzen(Clearn, resolution, pzn_win);
+
+% adjusting meshgrid size
+[X, Y] = meshgrid(x_a, y_a);
+
+nonparam_grid = MLparzen(p_a, p_b, p_c, X, Y);
+
+% plotting non-parametric estimate
+figure(12);
+hold on;
+title('Non-Parametric Estimation with ML Classification Boundaries');
+legend('Class A','Class B','Class C');
+xlabel('x1');
+ylabel('x2');
+title('Non-Parametric Estimation for 2D Case');
+sz = 20;
+scatter(Atest(:, 1), Atest(:, 2), sz, 'filled');
+scatter(Btest(:, 1), Btest(:, 2), sz, 'filled');
+scatter(Ctest(:, 1), Ctest(:, 2), sz, 'filled');
+contour(X, Y, nonparam_grid, 'color', 'black');
+legend('Class A', 'Class B', 'Class C', 'ML Decision Boundary');
+hold off;
+
 %%
 % PART 4
 %4.1 and 4.2
@@ -142,7 +231,7 @@ x = global_min_AB:1:global_max_AB;
 [Z1, err1] = sequential_classifier(X, Y, 0);
 [Z2, err2] = sequential_classifier(X, Y, 0);
 [Z3, err3] = sequential_classifier(X, Y, 0);
-figure(11);
+figure(13);
 scatter(classA_part4(:,1), classA_part4(:,2));
 hold on;
 scatter(classB_part4(:,1),classB_part4(:,2));
@@ -154,7 +243,7 @@ title('Sequential Classifier 1');
 legend('Class A','Class B');
 xlabel('x1');
 ylabel('x2');
-figure(12);
+figure(14);
 scatter(classA_part4(:,1), classA_part4(:,2));
 hold on;
 scatter(classB_part4(:,1),classB_part4(:,2));
@@ -166,7 +255,7 @@ title('Sequential Classifier 2');
 legend('Class A','Class B');
 xlabel('x1');
 ylabel('x2');
-figure(13);
+figure(15);
 scatter(classA_part4(:,1), classA_part4(:,2));
 hold on;
 scatter(classB_part4(:,1),classB_part4(:,2));
@@ -179,7 +268,7 @@ legend('Class A','Class B');
 xlabel('x1');
 ylabel('x2');
 %4.3
-figure(14)
+figure(16)
 [min, max, avg, std] = sequential_error_analysis(X, Y);
 scatter(min(:,1), min(:,2), 'r');
 hold on;
