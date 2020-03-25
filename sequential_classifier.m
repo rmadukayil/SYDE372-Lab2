545df64e09055147d3e81604f403d2cffb4149f4
Olivia S Y Ng <35663352+oliviang@users.noreply.github.com>
2020-03-24 23:30:30 -0400

    seq classifier returns combinational classifier

--- sequential_classifier.m
+++ sequential_classifier.m
@@ -1,14 +1,13 @@
-function [Z, error_rate] = sequential_classifier(X, Y, j_limit)
+function [G, error_rate] = sequential_classifier(X, Y, j_limit)
     %j_limit = 0 means no limit on j
     global classA_part4 classB_part4;
     classA_data = classA_part4;
     classB_data = classB_part4;
     nA = 0;
     nB = 0;
+    naB_nbA = int16.empty;
     a_prototypes = double.empty(0,2);
     b_prototypes = double.empty(0,2);
-    %Z = double.empty(size(X,1), size(X,1),0);
-    Z=double.empty;
     j = 0;
     
     while true
@@ -18,24 +17,15 @@ function [Z, error_rate] = sequential_classifier(X, Y, j_limit)
         j = j+1;
         a_prototypes = vertcat(a_prototypes, prototypeA);
         b_prototypes = vertcat(b_prototypes, prototypeB);
-        
-        Z_test = zeros(size(X));
-        for i=1:numel(X)
-            classified_A = MED(prototypeA, prototypeB, [X(i) Y(i)]);
-            if classified_A
-                Z_test(i) = 10; %%classify as A
-            else
-                Z_test(i) = 1; %%classify as B
-            end
-        end
-        Z = cat(3, Z, Z_test);
 
-        if AB_confusion(1,2) == 0 %naB= 0
-            %classA_data = setxor(classA_data, success_A);
+        naB = AB_confusion(1,2);
+        nbA = AB_confusion(2,1);
+        naB_nbA = vertcat(naB_nbA, [naB, nbA]);
+        
+        if naB == 0 
             classB_data = setxor(classB_data, success_B,'rows');
         end
-        if AB_confusion(2,1) == 0 %nbA = 0
-            %classB_data = setxor(classB_data, success_B);
+        if nbA == 0 
             classA_data = setxor(classA_data, success_A,'rows');
         end  
         if size(classA_data,1)== 0 || size(classB_data, 1) == 0
@@ -45,6 +35,21 @@ function [Z, error_rate] = sequential_classifier(X, Y, j_limit)
             break
         end
     end
+    %generate combinational sequential classifier  
+    G = zeros(size(X));
+    for k=1:numel(G)
+        for i=1:j
+            classified_A = MED(a_prototypes(i,:), b_prototypes(i,:), [X(k) Y(k)]);
+            if classified_A && naB_nbA(i,2)== 0 %%classified as 'A' AND nbA = 0
+                G(k) = 10; %%classify as A
+                break
+            elseif ~classified_A && naB_nbA(i,1)== 0 %%classified as 'B' AND naB = 0
+                G(k) = 1; %%classify as B
+                break
+            end
+        end
+    end
+    %error rate calculations
     for k=1:size(classA_part4,1) 
         x_bar = classA_part4(k,:);
         for i=1:j
@@ -70,5 +75,3 @@ function [Z, error_rate] = sequential_classifier(X, Y, j_limit)
     error_rate = 1-((nA+nB)/400);
     
     end
-   
-
