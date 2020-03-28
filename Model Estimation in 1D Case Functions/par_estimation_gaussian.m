423458f4fc5ad6f57f7eccbfd0db1da6c3b515f7
rmadukayil <43508700+rmadukayil@users.noreply.github.com>
2020-03-27 22:49:21 -0400

    Delete par_estimation_gaussian.m

--- Model Estimation in 1D Case Functions/par_estimation_gaussian.m
+++
@@ -1,9 +0,0 @@
-function [output] = par_estimation_gaussian(input_class)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-mu = sum(input_class,'all')/numel(input_class);
-x = (input_class - mu).^2;
-sigma = sum(x,'all')/numel(input_class);
-output = normpdf(input_class,mu,sigma);
-end
-
