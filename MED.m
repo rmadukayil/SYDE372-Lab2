e6f0d01859edd7a17c6aeca5a0fe3a8e30d0fc2a
rmadukayil <43508700+rmadukayil@users.noreply.github.com>
2020-03-27 22:49:59 -0400

    Delete MED.m

--- MED.m
+++
@@ -1,9 +0,0 @@
-function [classA] = MED(prototypeA, prototypeB, x)
-    dist_classA = sqrt((prototypeA(1)-x(1)).^2 + (prototypeA(2)-x(2)).^2);
-    dist_classB = sqrt((prototypeB(1)-x(1)).^2 + (prototypeB(2)-x(2)).^2);
-    if dist_classA < dist_classB
-        classA = true;
-    else
-        classA = false;
-    end
-end
